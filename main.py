e773e6c75ace1b7d992618fe2f2b98f6f30ed7d7
yulrio <33221012@std.stei.itb.ac.id>
2025-01-30 19:14:57 +0700

    update train test

--- main.py
+++ main.py
@@ -57,8 +57,8 @@ class Processor():
                 raise ValueError('Please appoint --load-weights.')
             self.recoder.print_log('Model:   {}.'.format(self.arg.model))
             self.recoder.print_log('Weights: {}.'.format(self.arg.load_weights))
-            train_wer = seq_eval(self.arg, self.data_loader["train_eval"], self.model, self.device,
-                                "train", 6667, self.arg.work_dir, self.recoder, self.arg.evaluate_tool)
+            # train_wer = seq_eval(self.arg, self.data_loader["train_eval"], self.model, self.device,
+            #                     "train", 6667, self.arg.work_dir, self.recoder, self.arg.evaluate_tool)
             dev_wer = seq_eval(self.arg, self.data_loader["dev"], self.model, self.device,
                                 "dev", 6667, self.arg.work_dir, self.recoder, self.arg.evaluate_tool)
             test_wer = seq_eval(self.arg, self.data_loader["test"], self.model, self.device,
